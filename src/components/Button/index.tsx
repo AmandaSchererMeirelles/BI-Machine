e97809e074036f8f84240fccb5ba587f46d2fa89
AmandaSchererMeirelles <mandy.floripa2015@hotmail.com>
2023-12-05 15:11:10 -0300

    tentativa de landing page

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,23 @@
+import { ButtonContainer } from "./styles";
+
+interface ButtonProps {
+  label: string;
+  type?: "button" | "submit" | "reset" | undefined;
+  width?: number;
+  height?: number;
+  onClick?: () => void;
+  variant?: 'primary' | 'danger' | 'secondary'
+}
+export function Button({
+  label,
+  width = 200,
+  height = 36,
+  onClick,
+  variant= "primary"
+}: ButtonProps) {
+  return (
+    <ButtonContainer width={width} height={height} onClick={onClick} variant={variant}>
+      {label}
+    </ButtonContainer>
+  );
+}
